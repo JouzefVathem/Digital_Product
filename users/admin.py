8a7f4e18947c34836a3c12dbdbeb5dc4609f601f
Seyed Reza Ahmadi <ahmadi_sr@yaftyar.ir>
2023-09-20 18:15:05 +0330

    Upgrading the view of the admin panel.

--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,24 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 from django.utils.translation import gettext_lazy as _
 
-from .models import User, Province
+from .models import User, Province, UserProfile, Device
+
+
+class ProvinceAdminInline(admin.ModelAdmin):
+    model = Province
+    fields = ['name']
+    extra = 0
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    fields = ['nick_name', 'avatar', 'birthday', 'gender']
+    verbose_name_plural = 'User Profile'
+    inlines = [ProvinceAdminInline]
+
+class DeviceInline(admin.StackedInline):
+    model = Device
+    fields = ['device_uuid', 'last_login', 'device_type', 'device_os', 'device_model', 'app_version']
+    verbose_name_plural = 'Device'
+    extra = 0
 
 class MyUserAdmin(UserAdmin):
     fieldsets = (
@@ -20,6 +37,7 @@ class MyUserAdmin(UserAdmin):
     list_display = ('username', 'phone_number', 'email', 'is_staff')
     search_fields = ('username__exact', )
     ordering = ('-id', )
+    inlines = [UserProfileInline, DeviceInline]
 
     def get_search_results(self, request, queryset, search_term):
         queryset, may_have_duplicates = super().get_search_results(request, queryset, search_term)
