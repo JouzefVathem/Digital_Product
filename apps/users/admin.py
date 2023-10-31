1b79a3b55e2864477ee027e9885a28b59a2809da
Seyed Reza Ahmadi <ahmadi_sr@yaftyar.ir>
2023-10-31 17:37:55 +0330

    Adding 'is_active' into list display admin panel.

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -10,17 +10,21 @@ class ProvinceAdminInline(admin.ModelAdmin):
     model = Province
     fields = ['name']
     extra = 0
+
+
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
     fields = ['nick_name', 'avatar', 'birthday', 'gender']
     verbose_name_plural = 'User Profile'
     inlines = [ProvinceAdminInline]
 
+
 class DeviceInline(admin.StackedInline):
     model = Device
     fields = ['device_uuid', 'last_login', 'device_type', 'device_os', 'device_model', 'app_version']
     verbose_name_plural = 'Device'
     extra = 0
+
 
 class MyUserAdmin(UserAdmin):
     fieldsets = (
@@ -32,49 +36,25 @@ class MyUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {'classes': ('wide',),
                 'fields': ('username', 'phone_number', 'password1', 'password2'),
-        }),
+                }),
     )
-    list_display = ('username', 'phone_number', 'email', 'is_staff')
-    search_fields = ('username__exact', )
-    ordering = ('-id', )
+    list_display = ('username', 'phone_number', 'email', 'is_staff', 'is_active')
+    search_fields = ('username__exact',)
+    ordering = ('-id',)
     inlines = [UserProfileInline, DeviceInline]
 
     def get_search_results(self, request, queryset, search_term):
         queryset, may_have_duplicates = super().get_search_results(request, queryset, search_term)
-        try :
+        try:
             search_term_as_int = int(search_term)
         except ValueError:
             pass
-        else :
+        else:
             queryset |= self.model.objects.filter(phone_number=search_term_as_int)
         return queryset, may_have_duplicates
-    
+
+
 admin.site.unregister(Group)
 admin.site.register(Province, ProvinceAdminInline)
 admin.site.register(User, MyUserAdmin)
 # admin.site.register(Site)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
