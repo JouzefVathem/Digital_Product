f5a02761fd0e7238f488f187504143316833a708
Seyed Reza Ahmadi <ahmadi_sr@yaftyar.ir>
2023-11-05 16:27:34 +0330

    Modify Imagefield to thumbnails Image field. Write an admin display method to show avatar by thumbnail.

--- apps/subscriptions/models.py
+++ apps/subscriptions/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
+from django.utils.html import format_html
+
+from thumbnails.fields import ImageField
 
 from utils.validators import validate_sku
 
@@ -9,7 +13,7 @@ class Package(models.Model):
     sku = models.CharField(_("stock keeping unit"), max_length=20,
                            help_text=_("Unique identifier for the package"), validators=[validate_sku], db_index=True)
     description = models.TextField(_("description"), blank=True)
-    avatar = models.ImageField(_("avatar"), upload_to="packages/", blank=True)
+    avatar = ImageField(_('avatar'), upload_to='Packages/', blank=True, null=True, pregenerated_sizes=['small'])
     is_enable = models.BooleanField(_("is enable"), default=True)
     price = models.PositiveIntegerField(_("price"))
     # gateways = models.ManyToManyField('payments.Gateway')
@@ -22,15 +26,28 @@ class Package(models.Model):
         verbose_name = _("Package")
         verbose_name_plural = _("Packages")
 
+    @admin.display(description='avatar')
+    def photo(self):
+        try:
+            small_url = self.avatar.thumbnails.small.url
+            original_url = self.avatar.url
+
+            return format_html(
+                f'<a href="{original_url}" target="_blank"> <img src="{small_url}" width=50 style="border-radius:50%; '
+                f'border: 3px solid gray; padding: 5px"/> </a>')
+
+        except ValueError:
+            return format_html(
+                '<strong style="color: whitesmoke; padding: 5px; border-radius: 5px; '
+                'background-color: #990100b5">⚠️ avatar not Found !!! </strong>')
+
     @staticmethod
     def make_enable(queryset):
-        queryset.filter(is_enable=False)
-        queryset.update(is_enable=True)
+        queryset.filter(is_enable=False).update(is_enable=True)
 
     @staticmethod
     def make_disable(queryset):
-        queryset.filter(is_enable=True)
-        queryset.update(is_enable=False)
+        queryset.filter(is_enable=True).update(is_enable=False)
 
     def __str__(self):
         return self.title
@@ -46,3 +63,6 @@ class Subscription(models.Model):
         db_table = 'subscriptions'
         verbose_name = _("Subscription")
         verbose_name_plural = _("Subscriptions")
+
+    def __str__(self):
+        return str(self.user.username) + ", " + str(self.package.title)
