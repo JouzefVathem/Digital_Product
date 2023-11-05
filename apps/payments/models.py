e277de1e1159a2d0da588bc206c8eaf50ce1b19f
Seyed Reza Ahmadi <ahmadi_sr@yaftyar.ir>
2023-11-05 14:05:47 +0330

    Adding "ID" and fields that we needed to each of the admin classes. Display the avatar of them that have this field.

--- apps/payments/models.py
+++ apps/payments/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.utils.html import format_html
+from django.contrib import admin
 
 from utils.validators import validate_phone_number
+
+from thumbnails.fields import ImageField
 
 
 class Gateway(models.Model):
     title = models.CharField(_("title"), max_length=100)
     description = models.TextField(_("discription"), blank=True)
-    avatar = models.ImageField(_("avatar"), upload_to="gateways/", blank=True)
+    avatar = ImageField(_('avatar'), upload_to='gateway/', blank=True, null=True, pregenerated_sizes=['small'])
     is_enable = models.BooleanField(_("is enable"), default=True)
     # credentials = models.JSONFieldField(_("credentials"), blank=True)
     created_time = models.DateTimeField(_("created time"), auto_now_add=True)
@@ -18,15 +22,32 @@ class Gateway(models.Model):
         verbose_name = _("Gateway")
         verbose_name_plural = _("Gateways")
 
+    @admin.display(description='Avatar')
+    def display_avatar(self):
+        try:
+            small_url = self.avatar.thumbnails.small.url
+            original_url = self.avatar.url
+
+            return format_html(
+                f'<a href="{original_url}" target="_blank"> '
+                f'<img src="{small_url}" width=50 style="border-radius:2rem; object-fit:contain"/> </a>')
+
+        except ValueError:
+            return format_html(
+                '<strong style="color: white-smoke;padding: 10px; border-top-left-radius: 20px; '
+                'border-bottom-right-radius: 20px; border-top-right-radius: 10px; border-bottom-left-radius: '
+                '10px; background-color: #B31312">avatar not Found !!!</strong>')
+
     @staticmethod
     def make_enable(queryset):
-        queryset.filter(is_enable=False)
-        queryset.update(is_enable=True)
+        queryset.filter(is_enable=False).update(is_enable=True)
 
     @staticmethod
     def make_disable(queryset):
-        queryset.filter(is_enable=True)
-        queryset.update(is_enable=False)
+        queryset.filter(is_enable=True).update(is_enable=False)
+
+    def __str__(self):
+        return self.title
 
 
 class Payment(models.Model):
@@ -55,7 +76,6 @@ class Payment(models.Model):
     package = models.ForeignKey("subscriptions.Package", verbose_name=_("package"), related_name='%(class)s',
                                 on_delete=models.CASCADE)
     gateway = models.ForeignKey(Gateway, verbose_name=_("gateway"), related_name='%(class)s', on_delete=models.CASCADE)
-    price = models.PositiveIntegerField(_("price"), default=0)
     status = models.PositiveSmallIntegerField(_("status"), choices=STATUS_CHOICES, default=STATUS_VOID, db_index=True)
     device_uuid = models.CharField(_("device uuid"), max_length=40, blank=True)
     token = models.CharField(_("token"), blank=True)
@@ -68,3 +88,10 @@ class Payment(models.Model):
         db_table = 'payments'
         verbose_name = _('Payment')
         verbose_name_plural = _("Payments")
+
+    @property
+    def price(self):
+        return self.package.price
+
+    def __str__(self):
+        return self.user.username
