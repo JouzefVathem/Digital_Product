9ce9f8432ede7f9dc3772f92c07e823840d45178
Seyed Reza Ahmadi <ahmadi_sr@yaftyar.ir>
2023-11-05 14:21:15 +0330

    Adding ordering by "ID" and "updated_time" field in admin panel.

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -5,8 +5,9 @@ from .models import Gateway, Payment
 
 @admin.register(Gateway)
 class GatewayAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'display_avatar', 'is_enable')
+    list_display = ('id', 'title', 'display_avatar', 'is_enable', 'updated_time')
     actions = ('make_enable', 'make_disable')
+    ordering = ('id',)
     verbose_name_plural = 'Gateway'
 
     def make_enable(self, request, queryset):
@@ -22,7 +23,9 @@ class GatewayAdmin(admin.ModelAdmin):
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'package', 'gateway', 'price', 'status', 'phone_number', 'created_time')
+    list_display = (
+        'id', 'user', 'package', 'gateway', 'price', 'status', 'phone_number', 'created_time', 'updated_time')
     list_filter = ('status', 'gateway', 'package')
     search_fields = ('user_username', 'phone_number')
+    ordering = ('id',)
     verbose_name_plural = 'Payment'
