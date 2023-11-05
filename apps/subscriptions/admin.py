e3172784bbb96f4fd822e4b7348cf13dc3fce2a0
Seyed Reza Ahmadi <ahmadi_sr@yaftyar.ir>
2023-11-05 16:21:31 +0330

    add the message actions and add "id" and "updated_time" to list display.

--- apps/subscriptions/admin.py
+++ apps/subscriptions/admin.py
@@ -1,29 +1,37 @@
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
+from django.contrib import messages
 
 from .models import Package, Subscription
 
 
 @admin.register(Package)
 class PackageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'sku', 'is_enable', 'price', 'duration')
+    list_display = ('id', 'title', 'photo', 'sku', 'is_enable', 'price', 'duration', 'created_time', 'updated_time')
+    ordering = ('id', )
     actions = ('make_enable', 'make_disable')
 
+    @admin.action(description='Enable selected Packages')
     def make_enable(self, request, queryset):
-        Package.make_enable(queryset)
+        enabled_count = len(queryset.filter(is_enable=False))
+        if enabled_count != 0:
+            messages.add_message(request, messages.SUCCESS,
+                                 f'{enabled_count} of the disable Packages enabled.')
 
-    make_enable.short_description = "Enable selected Packages"
+            Package.make_enable(queryset)
 
+    @admin.action(description='Disable selected Packages')
     def make_disable(self, request, queryset):
-        Package.make_disable(queryset)
+        disabled_count = len(queryset.filter(is_enable=True))
+        if disabled_count != 0:
+            messages.add_message(request, messages.SUCCESS,
+                                 f'{disabled_count} of the enable Packages disabled.')
+
+            Package.make_disable(queryset)
 
     make_disable.short_description = "Disable selected Packages"
-
-    # def dt(self):
-    #     queryset = self.objects.filter()
-    #     self.make_enable(queryset)
 
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'package', 'created_time', 'expire_time')
+    list_display = ('id', 'user', 'package', 'created_time', 'expire_time')
+    ordering = ('expire_time',)
