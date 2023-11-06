a7196e3c6843d7b21b6f1f3694953e756f732b92
Seyed Reza Ahmadi <ahmadi_sr@yaftyar.ir>
2023-11-06 15:58:48 +0330

    First steps for use "DefaultRouter" on project (NOTE : This commit is not complete, just for push to git).

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -3,26 +3,25 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from .views import (
-    ProductListView, ProductDetailView,
-    CategoryListView, CategoryDetailView,
-    FileListView, FileDetailView, ActiveProductList
+    ProductViewSet, ActiveProductList,  # ProductListView, ProductDetailView,
+    CategoryViewSet,  # CategoryListView, CategoryDetailView,
+    FileViewSet,  # FileListView, FileDetailView,
 )
 
 router = DefaultRouter()
-router.register(r'active-products', ActiveProductList, basename='products')
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
+router.register(r'products', ProductViewSet)
+router.register(r'active-users-products', ActiveProductList, basename='active-products')
+router.register(r'categories', CategoryViewSet)
+router.register(r'files', FileViewSet)
 
 urlpatterns = [
-    path('categories/', CategoryListView.as_view(), name='category-list'),
-    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
-
-    path('products/', ProductListView.as_view(), name='product-list'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-
-    path('products/<int:product_id>/files/', FileListView.as_view(), name='file-list'),
-    path('products/<int:product_id>/files/<int:pk>/', FileDetailView.as_view(), name='file-detail'),
-    path('', include(router.urls))
+    # path('categories/', CategoryListView.as_view(), name='category-list'),
+    # path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
+    #
+    # path('products/', ProductListView.as_view(), name='product-list'),
+    # path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    #
+    # path('products/<int:product_id>/files/', FileListView.as_view(), name='file-list'),
+    # path('products/<int:product_id>/files/<int:pk>/', FileDetailView.as_view(), name='file-detail'),
+    path('Product/', include(router.urls))
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -14,156 +14,171 @@ from .models import Category, Product, File
 from .serializers import CategorySerializer, ProductSerializer, FileSerializer
 
 
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
 class ActiveProductList(viewsets.ReadOnlyModelViewSet):
     queryset = Product.get_active_users_products()
     serializer_class = ProductSerializer
 
 
-class CategoryListView(APIView):
-
-    def get(self, request):
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True, context={'request': request})
-        return Response(data=serializer.data)
-
-    def post(self, request):
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
 
-class CategoryDetailView(APIView):
-
-    def get_object(self, pk):
-        try:
-            category = Category.objects.get(pk=pk)
-        except Category.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        return category
-
-    def get(self, request, pk):
-        category = self.get_object(pk)
-        serializer = CategorySerializer(category, context={'request': request})
-        return Response(data=serializer.data)
-
-    def put(self, request, pk):
-        category = self.get_object(pk)
-        serializer = CategorySerializer(category, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        category = self.get_object(pk)
-        category.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class FileViewSet(viewsets.ModelViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
 
 
-class ProductListView(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
-    def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True, context={'request': request})
-        return Response(data=serializer.data)
-
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ProductDetailView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get_object(self, pk):
-
-        try:
-            product = Product.objects.get(pk=pk)
-        except Product.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        return product
-
-    def get(self, request, pk):
-        if not Subscription.objects.filter(
-                user=request.user,
-                expire_time__gt=timezone.now()
-        ).exists():
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-        product = self.get_object(pk)
-        serializer = ProductSerializer(product, context={'request': request})
-        return Response(data=serializer.data)
-
-    def put(self, request, pk):
-        if not Subscription.objects.filter(
-                user=request.user,
-                expire_time__gt=timezone.now()
-        ).exists():
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-        product = self.get_object(pk)
-        serializer = ProductSerializer(product, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        if not Subscription.objects.filter(
-                user=request.user,
-                expire_time__gt=timezone.now()
-        ).exists():
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-        product = self.get_object(pk)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# class CategoryListView(APIView):
+#
+#     def get(self, request):
+#         categories = Category.objects.all()
+#         serializer = CategorySerializer(categories, many=True, context={'request': request})
+#         return Response(data=serializer.data)
+#
+#     def post(self, request):
+#         serializer = CategorySerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class FileListView(APIView):
+# class CategoryDetailView(APIView):
+#
+#     def get_object(self, pk):
+#         try:
+#             category = Category.objects.get(pk=pk)
+#         except Category.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         return category
+#
+#     def get(self, request, pk):
+#         category = self.get_object(pk)
+#         serializer = CategorySerializer(category, context={'request': request})
+#         return Response(data=serializer.data)
+#
+#     def put(self, request, pk):
+#         category = self.get_object(pk)
+#         serializer = CategorySerializer(category, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk):
+#         category = self.get_object(pk)
+#         category.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    def get(self, request, product_id):
-        files = File.objects.filter(product_id=product_id)
-        serializer = FileSerializer(files, many=True, context={'request': request})
-        return Response(data=serializer.data)
-
-    def post(self, request, product_id):
-        serializer = FileSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save(product_id=product_id)
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class FileDetailView(APIView):
-
-    def get_object(self, pk):
-        try:
-            file = File.objects.get(pk=pk)
-        except File.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        return file
-
-    def get(self, request, product_id, pk):
-        file = self.get_object(pk)
-        serializer = FileSerializer(file, context={'request': request})
-        return Response(data=serializer.data)
-
-    def put(self, request, product_id, pk):
-        file = self.get_object(product_id, pk)
-        serializer = FileSerializer(file, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, product_id, pk):
-        file = self.get_object(product_id, pk)
-        file.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#
+# class ProductListView(APIView):
+#     permission_classes = [IsAuthenticatedOrReadOnly]
+#
+#     def get(self, request):
+#         products = Product.objects.all()
+#         serializer = ProductSerializer(products, many=True, context={'request': request})
+#         return Response(data=serializer.data)
+#
+#     def post(self, request):
+#         serializer = ProductSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class ProductDetailView(APIView):
+#     permission_classes = [IsAuthenticated]
+#
+#     def get_object(self, pk):
+#
+#         try:
+#             product = Product.objects.get(pk=pk)
+#         except Product.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         return product
+#
+#     def get(self, request, pk):
+#         if not Subscription.objects.filter(
+#                 user=request.user,
+#                 expire_time__gt=timezone.now()
+#         ).exists():
+#             return Response(status=status.HTTP_401_UNAUTHORIZED)
+#
+#         product = self.get_object(pk)
+#         serializer = ProductSerializer(product, context={'request': request})
+#         return Response(data=serializer.data)
+#
+#     def put(self, request, pk):
+#         if not Subscription.objects.filter(
+#                 user=request.user,
+#                 expire_time__gt=timezone.now()
+#         ).exists():
+#             return Response(status=status.HTTP_401_UNAUTHORIZED)
+#
+#         product = self.get_object(pk)
+#         serializer = ProductSerializer(product, data=request.data, context={'request': request})
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk):
+#         if not Subscription.objects.filter(
+#                 user=request.user,
+#                 expire_time__gt=timezone.now()
+#         ).exists():
+#             return Response(status=status.HTTP_401_UNAUTHORIZED)
+#
+#         product = self.get_object(pk)
+#         product.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
+#
+# class FileListView(APIView):
+#
+#     def get(self, request, product_id):
+#         files = File.objects.filter(product_id=product_id)
+#         serializer = FileSerializer(files, many=True, context={'request': request})
+#         return Response(data=serializer.data)
+#
+#     def post(self, request, product_id):
+#         serializer = FileSerializer(data=request.data, context={'request': request})
+#         if serializer.is_valid():
+#             serializer.save(product_id=product_id)
+#             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class FileDetailView(APIView):
+#
+#     def get_object(self, pk):
+#         try:
+#             file = File.objects.get(pk=pk)
+#         except File.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#         return file
+#
+#     def get(self, request, product_id, pk):
+#         file = self.get_object(pk)
+#         serializer = FileSerializer(file, context={'request': request})
+#         return Response(data=serializer.data)
+#
+#     def put(self, request, product_id, pk):
+#         file = self.get_object(product_id, pk)
+#         serializer = FileSerializer(file, data=request.data, context={'request': request})
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, product_id, pk):
+#         file = self.get_object(product_id, pk)
+#         file.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
