872efb13108c62e2f34ed4f19e56a94b178917bb
Seyed Reza Ahmadi <ahmadi_sr@yaftyar.ir>
2023-11-08 17:26:58 +0330

    Add more ModelAdmin attributes.

--- apps/subscriptions/admin.py
+++ apps/subscriptions/admin.py
@@ -1,14 +1,32 @@
 from django.contrib import admin
 from django.contrib import messages
 
+from django.utils.translation import gettext_lazy as _
+
 from .models import Package, Subscription
+
+from import_export.admin import ImportExportModelAdmin
 
 
 @admin.register(Package)
-class PackageAdmin(admin.ModelAdmin):
+class PackageAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    readonly_fields = ('created_time', 'updated_time')
+    fieldsets = (
+        (None, {'classes': ('wide',), 'fields': (('title', 'is_enable'),)}),
+        (_('Package Info'), {'classes': ('wide',), 'fields': ('description', 'avatar')}),
+        (_('Optional Info'), {'classes': ('wide',), 'fields': ('sku', 'price', 'duration')}),
+        (_('Create, Update'), {'fields': ('created_time', 'updated_time')}),
+    )
+    ordering = ('duration', )
     list_display = ('id', 'title', 'photo', 'sku', 'is_enable', 'price', 'duration', 'created_time', 'updated_time')
-    ordering = ('id', )
+    list_display_links = ('id', 'title')
+    list_filter = ('is_enable',)
+    list_per_page = 15
+    search_fields = ('id', 'title', 'user')
+    date_hierarchy = 'updated_time'
     actions = ('make_enable', 'make_disable')
+    verbose_name_plural = 'Packages'
+    search_help_text = _('Search by id, title or user')
 
     @admin.action(description='Enable selected Packages')
     def make_enable(self, request, queryset):
@@ -28,10 +46,21 @@ class PackageAdmin(admin.ModelAdmin):
 
             Package.make_disable(queryset)
 
-    make_disable.short_description = "Disable selected Packages"
-
 
 @admin.register(Subscription)
-class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'package', 'created_time', 'expire_time')
+class SubscriptionAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    readonly_fields = ('created_time',)
+    fieldsets = (
+        (None, {'classes': ('wide',), 'fields': (('user', 'package'),)}),
+        (_('Expire Time'), {'fields': ('created_time', 'expire_time')}),
+    )
     ordering = ('expire_time',)
+    list_display = ('id', 'created_time', 'expire_time', 'user', 'package')
+    list_display_links = ('id', 'created_time', 'expire_time')
+    list_filter = ('user', 'package')
+    list_select_related = ('user', 'package')
+    list_per_page = 15
+    search_fields = ('id', 'user', 'package')
+    date_hierarchy = 'expire_time'
+    verbose_name_plural = 'Subscriptions'
+    search_help_text = _('Search by id, user or package')
