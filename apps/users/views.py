030703e52b1098ef5d015d43fe4b5507f00b2798
Seyed Reza Ahmadi <ahmadi_sr@yaftyar.ir>
2023-11-12 16:10:36 +0330

    Making correct the Imports and adding "device.save()" to create th device.

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,12 +1,13 @@
-import random, uuid
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+import random
+import uuid
 
 from django.core.cache import cache
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import User, Device
+
 
 class RegisterView(APIView):
 
@@ -14,26 +15,26 @@ class RegisterView(APIView):
         phone_number = request.data.get('phone_number')
         if not phone_number:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        try :
-            user = User.objects.get(phone_number=phone_number)
+        try:
+            User.objects.get(phone_number=phone_number)
             return Response({'detail': 'User already registered!'}, status=status.HTTP_400_BAD_REQUEST)
         except User.DoesNotExist:
             code = random.randint(10000, 99999)
 
-            # send message
-            # cache 
+            # send message number
+            # cache
             cache.set(str(phone_number), code, 2 * 60)
 
+            # user = User.objects.get_or_create(phone_number=phone_number)
+            # user.save()
+
             return Response({'code': code})
-        
-        # user, created = User.objects.get_or_create(phone_number=phone_number)
-        
 
 
 class GetTokenView(APIView):
-    
-    def post (self, request):
-        
+
+    def post(self, request):
+
         phone_number = request.data.get('phone_number')
         code = request.data.get('code')
         cached_code = cache.get(str(phone_number))
@@ -43,4 +44,5 @@ class GetTokenView(APIView):
             user = User.objects.create_user(phone_number=phone_number)
             token = str(uuid.uuid4())
             device = Device.objects.create(user=user)
+            device.save()
             return Response({'token': token})
