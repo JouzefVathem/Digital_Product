e180904c6715ff4bce2095cdfd6ad384f90667f8
Seyed Reza Ahmadi <ahmadi_sr@yaftyar.ir>
2023-11-12 15:45:20 +0330

    Defining the "DEFAULT_PAGINATION_CLASS" and adding "rest_framework.authentication.SessionAuthentication" to "DEFAULT_AUTHENTICATION_CLASSES" in REST_FRAMEWORK part setting file.

--- digital_product/settings.py
+++ digital_product/settings.py
@@ -168,10 +168,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 7,
+
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
