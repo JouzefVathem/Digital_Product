77b9c8caf2a912ae210d59dfdfeca7514f33a78f
Seyed Reza Ahmadi <ahmadi_sr@yaftyar.ir>
2023-11-15 10:57:15 +0330

    Adding generic APIs for models(List, detail).

--- apps/products/views.py
+++ apps/products/views.py
@@ -9,7 +9,6 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('-created_time')
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
-    http_method_names = ["get", "post"]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -19,22 +18,74 @@ class ActiveProductList(viewsets.ReadOnlyModelViewSet):
     queryset = Product.get_active_users_products().order_by('-created_time')
     serializer_class = ProductSerializer
     permission_classes = [IsAdminUser]
-    http_method_names = ["get"]
+    http_method_names = ['get']
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all().order_by('-created_time')
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated]
-    http_method_names = ["get", "post"]
 
 
 class FileViewSet(viewsets.ModelViewSet):
     queryset = File.objects.all().order_by('-created_time')
     serializer_class = FileSerializer
     permission_classes = [IsAuthenticated]
-    http_method_names = ["get", "post"]
 
+# --------------------------------------------------------------#
+# Generics Views
+
+
+# class ProductListView(generics.ListCreateAPIView):
+#     queryset = Product.objects.all().order_by('-created_time')
+#     serializer_class = ProductSerializer
+#     permission_classes = [IsAuthenticated]
+#     http_method_names = ['get', 'post']
+#
+#
+# class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     permission_classes = [IsAuthenticated]
+#     http_method_names = ['get', 'put', 'delete']
+#
+#
+# class ActiveProductList(viewsets.ReadOnlyModelViewSet):
+#     queryset = Product.get_active_users_products().order_by('-created_time')
+#     serializer_class = ProductSerializer
+#     permission_classes = [IsAdminUser]
+#     http_method_names = ['get']
+#
+#
+# class CategoryListView(generics.ListCreateAPIView):
+#     queryset = Category.objects.all().order_by('-created_time')
+#     serializer_class = CategorySerializer
+#     permission_classes = [IsAuthenticated]
+#     http_method_names = ['get', 'post']
+#
+#
+# class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+#     permission_classes = [IsAuthenticated]
+#     http_method_names = ['get', 'put', 'delete']
+#
+#
+# class FileListView(generics.ListCreateAPIView):
+#     queryset = File.objects.all().order_by('-created_time')
+#     serializer_class = FileSerializer
+#     permission_classes = [IsAuthenticated]
+#     http_method_names = ['get', 'post']
+#
+#
+# class FileDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = File.objects.all()
+#     serializer_class = FileSerializer
+#     permission_classes = [IsAuthenticated]
+#     http_method_names = ['get', 'put', 'delete']
+
+# -----------------------------------------------------------------#
+# API Views
 
 # class CategoryListView(APIView):
 #
