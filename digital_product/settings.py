aa1879941e307937e3a2c14052d3894cf0ede0c9
Seyed Reza Ahmadi <ahmadi_sr@yaftyar.ir>
2023-11-16 15:59:30 +0330

    Install and config "django-filter".

--- digital_product/settings.py
+++ digital_product/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'admin_auto_filters',
+    'django_filters',
 
     'rest_framework',
     'rest_framework_simplejwt',
